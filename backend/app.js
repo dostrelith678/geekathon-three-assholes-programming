48e36f82cc7f7e7e749d1707bb1e8fdc802a3f82
dostrelith678 <damjan.ostrelic@gmail.com>
2023-11-11 15:09:20 +0000

    Init OpenAI

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,13 @@ const admin = require("firebase-admin");
 const cors = require('cors');
 const serviceAccount = require("./serviceAccountKey.json");
 const paypal = require('paypal-rest-sdk');  // Add PayPal library
+const { OpenAI } = require('openai')
+
+// Initialize ChatGPT
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -106,21 +113,65 @@ app.post('/login', async (req, res) => {
     }
 });
 
-
 // Create an AI clone
-app.post('/create-clone', (req, res) => {
-    // Handle clone creation, Steamship API, and Firebase storage logic
+app.post('/create-clone', async (req, res) => {
+    try {
+        const aiCloneData = req.body;
+
+        // Validate request data (you can customize this based on your requirements)
+        if (!aiCloneData || typeof aiCloneData !== 'object') {
+            return res.status(400).json({ error: 'Invalid request body' });
+        }
+        // Generate a unique clone ID for the AI clone
+        const cloneId = db.ref('aiClones').push().key;
+
+        // Store AI clone data in the Realtime Database
+        const databaseRef = db.ref(`aiClones/${cloneId}`);
+        await databaseRef.set(aiCloneData);
+
+        // Return success response
+        return res.status(201).json({ message: 'AI clone created successfully', cloneId });
+    } catch (error) {
+        console.error('Error creating AI clone:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
-// Handle PayPal payment
-app.post('/paypal-payment', (req, res) => {
-    // Handle PayPal payment and update user in Firebase
+
+// Send chat message to AI clone and get response
+app.post('/get-chat-response', async (req, res) => {
+    try {
+        const { userMessage, cloneId } = req.body;
+
+        // Validate request data
+        if (!chatMessage || !userId) {
+            return res.status(400).json({ error: 'Invalid request body' });
+        }
+
+        // Enrich the chatMessage with a pre-prepared prompt from the clone data (replace with your logic)
+        const clonePrompt = getCloneSystemPrompt(cloneId);
+
+        // Make request to ChatGPT 4.0
+        const chatCompletion = await openai.chat.completions.create({
+            messages: [
+                { role: "system", content: clonePrompt },
+                { role: 'user', content: userMessage }
+            ],
+            model: 'gpt-4', //model: 'gpt-4-0613'
+        });
+
+
+        // Extract the generated response from ChatGPT
+        const generatedResponse = chatGptResponse.data.choices[0]?.text;
+
+        // Return the generated response to the client
+        return res.status(200).json({ generatedResponse });
+    } catch (error) {
+        console.error('Error getting chat response:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
-// Send chat message to AI clone
-app.post('/send-chat-message', (req, res) => {
-    // Handle sending messages to AI clone and responding to the client
-});
 
 // Generate AI selfie
 app.post('/generate-selfie', (req, res) => {
@@ -130,6 +181,11 @@ app.post('/generate-selfie', (req, res) => {
 // Get all clones available
 app.get('/get-all-clones', (req, res) => {
     // Fetch data from Firebase and send it back to the client
+});
+
+// Handle PayPal payment
+app.post('/paypal-payment', (req, res) => {
+    // Handle PayPal payment and update user in Firebase
 });
 
 // Start the server
