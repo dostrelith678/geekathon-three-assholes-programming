d114b9f26e84b3a73a69497ecdbedabde940171a
victor-schumann <2220013@my.ipleiria.pt>
2023-11-11 11:35:31 +0000

    advance on mockup design for login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -2,30 +2,29 @@
 
 import React from 'react';
 import styles from '../styles/login.module.css';
-// import { useMemo } from 'react'; // Assurez-vous que le chemin est correct
 
 export default function LoginPage() {
-    // const chatUUID = useMemo(() => uuidv4(), []);
-    // const { messages, input, handleInputChange, handleSubmit } = useChat({
-    //     id: chatUUID,
-    //     body: { id: chatUUID },
-    //     api: '/api/steamship/chat'
-    // });
 
     return (
         <div className={styles.App}>
-            <div className={styles.topSection}><h1>DOPPLER</h1></div>
+            <div className={styles['top-section']}><h1>DOPPELCHAT</h1></div>
             <div className={styles['image-section']}></div>
             <div className={styles['login-section']}>
                 <h2 className={styles.heading}>Login</h2>
                 <form className={styles.form}>
-                    <label className={styles.label} htmlFor="username">Username:</label>
-                    <input className={styles.input} type="text" id="username" name="username"/>
+                    <div className={styles.box}>
+                        <input className={styles.input} type="text" id="email" name="email"/>
+                    </div>
 
-                    <label className={styles.label} htmlFor="password">Password:</label>
-                    <input className={styles.input} type="password" id="password" name="password"/>
-                    <div className={styles['button-container']}>
+                    <div className={styles.box}>
+                        <input className={styles.input} type="password" id="password" name="password"/>
+                    </div>
+
+                    <div className={styles.box}>
                         <button className={styles.button} type="submit">Login</button>
+                    </div>
+
+                    <div className={styles.box}>
                         <button className={styles.button} type="submit">Sign Up</button>
                     </div>
                 </form>
